ba44b8412d9ba214e17642014fb16f3053a9a850
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2022-07-13 14:35:50 -0400

    Used path to get file name instead of string conversion

--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 // target directory that hosts the files, current extesion to be changed,
 // and the required extension
 ((dir_name, current_extension, target_extension) => {
-  const fs = require('fs');
-  const path = require('path');
-  
+  const fs = require("fs");
+  const path = require("path");
+
   fs.readdir(dir_name, (err, files) => {
     if (err) return err;
 
-    files.forEach(file => {
-      const fileName = file.toString().split('.').slice(0, -1).join('.');
+    files.forEach((file) => {
+      // const fileName = file.toString().split('.').slice(0, -1).join('.');
+      const name = path.basename(file);
       const extension = path.extname(file);
-
-      if (extension === '.' + current_extension) {
+      const fileName = path.basename(name, extension);
+      // practising node scripting, just wanted to make it work a diferent way using path
+      if (extension === "." + current_extension) {
         const newFile = `${dir_name}${fileName}.${target_extension}`;
 
         fs.rename(`${dir_name}${file}`, newFile, (err) => {
-          if ( err ) return err;
-          console.log('Renamed', file, 'to', newFile);
+          if (err) return err;
+          console.log("Renamed", file, "to", newFile);
         });
       }
     });
   });
-})('./', 'js', 'jsx');
+})("./src/sass/", "css", "scss");
